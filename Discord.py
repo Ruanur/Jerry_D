37664d2eb13ec1e8d0d45ba16c0d368b98718df1
Ruanur <91407973+Ruanur@users.noreply.github.com>
2021-09-28 15:25:13 +0900

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -73,7 +73,7 @@ def play_next(ctx):
     if len(user) >= 1:
         if not vc.is_playing():
             del musicnow[0]
-            URL = song_queue[999999]
+            URL = song_queue[0]
             del user[0]
             del musictitle[0]
             del song_queue[0]
